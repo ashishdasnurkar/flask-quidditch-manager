fcac8dfa342459d2f98ed4765ae7a921bf671cee
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2019-08-18 09:54:53 +1000

    added graphql endpoint to test graphql queries

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_graphql import GraphQLView
+from schema import schema
 
 app = Flask(__name__)
 
@@ -6,6 +8,8 @@ app = Flask(__name__)
 
 def home():
   return "Hello World"
+
+app.add_url_rule("/graphql", view_func=GraphQLView.as_view("graphql", schema=schema, graphiql=True))
 
 if __name__ == "__main__":
   app.run(debug=True)
